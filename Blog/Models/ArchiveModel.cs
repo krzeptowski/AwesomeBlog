dc7a0eb2ec56df0f26b7266c69a95c298c839413
administration <administration@github.com>
2011-05-29 21:36:42 +0200

    poprawka

---
+++ Blog/Models/ArchiveModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+//using System.ComponentModel;
+//using System.ComponentModel.DataAnnotations;
+
+namespace Blog.Models
+{
+    public class ArchiveModel
+    {
+        public int month { get; set; }
+        public int year { get; set; }
+        public int count { get; set; }
+    }
+}
