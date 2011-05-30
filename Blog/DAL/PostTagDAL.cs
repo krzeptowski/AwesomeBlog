f19b1a773548711b35765a21a5e367d7912f2e79
administration <administration@github.com>
2011-05-30 19:58:41 +0200

    a

--- Blog/DAL/PostTagDAL.cs
+++ Blog/DAL/PostTagDAL.cs
@@ -18,6 +18,36 @@ namespace Blog.DAL
 
     public class PostTagDAL : IPostTag
     {
+
+        public List<Models.PostTagModel> pobierzPorcjePoDacie(DateTime data, int ile, int offset)
+        {
+            using (LinqTodbBlogDataContext db = new LinqTodbBlogDataContext())
+            {
+                try
+                {
+                    var lista = (from a in db.PostyTagis
+                                 where a.status != 0 && a.data_dodania.Date == data.Date
+                                 orderby a.data_dodania descending
+                                 select new Models.PostTagModel
+                                 {
+                                     Tytul = a.tytul,
+                                     Tresc = a.tresc,
+                                     Status = a.status,
+                                     Keywords = a.keywords,
+                                     Id = a.id,
+                                     Desc = a.description,
+                                     DataModyfikacji = a.data_modyfikacji,
+                                     DataDodania = a.data_dodania,
+                                 }).Skip(ile * offset).Take(ile).ToList();
+                    return lista;
+                }
+                catch (Exception)
+                {
+                    throw new Exception("Wystąpił błąd podczas pobierania wpisów");
+                }
+            }
+        }
+
         public int dodajPost(Models.PostTagModel model)
         {
             using (LinqTodbBlogDataContext db = new LinqTodbBlogDataContext())
@@ -55,7 +85,6 @@ namespace Blog.DAL
                 }
             }
         }
-
         public List<Models.PostTagModel> pobierzPorcje(int ile, int offset)
         {
             using (LinqTodbBlogDataContext db = new LinqTodbBlogDataContext())
@@ -84,65 +113,34 @@ namespace Blog.DAL
                 }
             }
         }
-
-        public List<Models.PostTagModel> pobierzPorcjePoDacie(DateTime data, int ile, int offset)
-　　　　{
-　　　　　　using (LinqTodbBlogDataContext db = new LinqTodbBlogDataContext())
-　　　　　　{
-　　　　　　　　try
-　　　　　　　　{
-　　　　　　　　　　var lista = (from a in db.PostyTagis
-　　　　　　　　　　　　　　　　 where a.status != 0 && a.data_dodania.Date == data.Date
-　　　　　　　　　　　　　　　　 orderby a.data_dodania descending
-　　　　　　　　　　　　　　　　 select new Models.PostTagModel
-　　　　　　　　　　　　　　　　 {
-　　　　　　　　　　　　　　　　　　 Tytul = a.tytul,
-　　　　　　　　　　　　　　　　　　 Tresc = a.tresc,
-　　　　　　　　　　　　　　　　　　 Status = a.status,
-　　　　　　　　　　　　　　　　　　 Keywords = a.keywords,
-　　　　　　　　　　　　　　　　　　 Id = a.id,
-　　　　　　　　　　　　　　　　　　 Desc = a.description,
-　　　　　　　　　　　　　　　　　　 DataModyfikacji = a.data_modyfikacji,
-　　　　　　　　　　　　　　　　　　 DataDodania = a.data_dodania,
-　　　　　　　　　　　　　　　　 }).Skip(ile * offset).Take(ile).ToList();
-　　　　　　　　　　return lista;
-　　　　　　　　}
-　　　　　　　　catch (Exception)
-　　　　　　　　{
-　　　　　　　　　　throw new Exception("Wystąpił błąd podczas pobierania wpisów");
-　　　　　　　　}
-　　　　　　}
-　　　　}
-
-        public List<Models.PostTagModel> pobierzPorcje(int ile, int offset)
-　　　　{
-　　　　　　using (LinqTodbBlogDataContext db = new LinqTodbBlogDataContext())
-　　　　　　{
-　　　　　　　　try
-　　　　　　　　{
-　　　　　　　　　　var lista = (from a in db.PostyTagis
-　　　　　　　　　　　　　　　　 where a.status != 0
-　　　　　　　　　　　　　　　　 orderby a.data_dodania descending
-　　　　　　　　　　　　　　　　 select new Models.PostTagModel
-　　　　　　　　　　　　　　　　 {
-　　　　　　　　　　　　　　　　　　 Tytul = a.tytul,
-　　　　　　　　　　　　　　　　　　 Tresc = a.tresc,
-　　　　　　　　　　　　　　　　　　 Status = a.status,
-　　　　　　　　　　　　　　　　　　 Keywords = a.keywords,
-　　　　　　　　　　　　　　　　　　 Id = a.id,
-　　　　　　　　　　　　　　　　　　 Desc = a.description,
-　　　　　　　　　　　　　　　　　　 DataModyfikacji = a.data_modyfikacji,
-　　　　　　　　　　　　　　　　　　 DataDodania = a.data_dodania,
-　　　　　　　　　　　　　　　　 }).Skip(ile * offset).Take(ile).ToList();
-　　　　　　　　　　return lista;
-　　　　　　　　}
-　　　　　　　　catch (Exception)
-　　　　　　　　{
-　　　　　　　　　　throw new Exception("Wystąpił błąd podczas pobierania wpisów");
-　　　　　　　　}
-　　　　　　}
-　　　　}
-
+        public List<Models.PostTagModel> pobierzArchiwum(int year, int month)
+        {
+            using (LinqTodbBlogDataContext db = new LinqTodbBlogDataContext())
+            {
+                try
+                {
+                    var lista = (from a in db.PostyTagis
+                                 where a.status != 0 && a.data_dodania.Year == year && a.data_dodania.Month == month
+                                 orderby a.data_dodania descending
+                                 select new Models.PostTagModel
+                                 {
+                                     Tytul = a.tytul,
+                                     Tresc = a.tresc,
+                                     Status = a.status,
+                                     Keywords = a.keywords,
+                                     Id = a.id,
+                                     Desc = a.description,
+                                     DataModyfikacji = a.data_modyfikacji,
+                                     DataDodania = a.data_dodania,
+                                 }).ToList();
+                    return lista;
+                }
+                catch (Exception)
+                {
+                    throw new Exception("Wystąpił błąd podczas pobierania wpisów");
+                }
+            }
+        }
         public bool usunPost(int id)
         {
             using (LinqTodbBlogDataContext db = new LinqTodbBlogDataContext())
